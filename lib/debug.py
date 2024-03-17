482762ca72ea9e80fe67e9221df48195782e508f
kaan <kabuke13@gmail.com>
2024-03-17 23:11:01 +0000

    allows us to create charity table

--- lib/debug.py
+++ lib/debug.py
@@ -3,11 +3,15 @@
 
 from models.__init__ import CONN, CURSOR
 from models.donor import Donor
+from models.charity import Charity
 import ipdb
 
 def reset_database():
     Donor.drop_table()
     Donor.create_table()
+
+    Charity.drop_table()
+    Charity.create_table()
 
 
 
